ca824467b1b2c39e536cbb3a9b4fcdd6b04ddd41
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-05 22:40:32 -0700

    Finish vampire.

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,18 @@
+class Vampire {
+  constructor(name, pet){
+    this.name = name;
+    this.pet = pet || 'bat';
+    this.thirsty = true
+    this.ouncesDrank = 0;
+  }
+  drink(){
+    this.thirsty = false
+    if(this.ouncesDrank > 40) {
+      return "I'm too full to drink anymore!"
+    } else {
+      this.ouncesDrank += 10
+    }
+  }
+}
+
+module.exports = Vampire
