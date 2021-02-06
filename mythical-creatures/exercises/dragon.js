9fd5b5bf4e11fd802a27302a420b26d0f45c1413
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-05 22:33:15 -0700

    Finish dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.timesEaten = 0;
+  }
+  greet(){
+    return (`Hi, ${this.rider}!`)
+  }
+  eat(){
+    this.timesEaten+=1
+    if(this.timesEaten > 2){
+      this.hungry = false
+    }
+  }
+}
 
+module.exports = Dragon
