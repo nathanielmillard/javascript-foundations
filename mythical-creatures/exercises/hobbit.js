e3f1db3528c907bfda018460d858d42e99d90a15
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-06 14:30:26 -0700

    Finish hobbit.

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,28 @@
+class Hobbit {
+  constructor(hobbit){
+    this.name = hobbit.name;
+    this.age = 0;
+    this.adult = false
+    this.old = false
+    this.hasRing = false
+  }
+  celebrateBirthday(){
+    this.age += 1
+    if (this.age > 32){
+      this.adult = true
+    }
+    if (this.age > 100){
+      this.old = true
+    }
+  }
+  getRing(){
+    if(this.name === 'Frodo'){
+      this.hasRing = true
+      return "Here is the ring!"
+    } else {
+      return "You can't have it!"
+    }
+  }
+}
+
+module.exports = Hobbit
