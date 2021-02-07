82db4b46e29c2efe781ffc2cf936daff9a3f1638
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-06 19:34:20 -0700

    Finish medusa.

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -4,6 +4,16 @@ var Statue = require('./statue');
 class Medusa {
   constructor(name){
     this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim){
+    if(this.statues.length < 3) {
+      this.statues.push( new Statue(victim.name))
+    } else {
+      let statue = this.statues.shift()
+      this.statues.push( new Statue(victim.name))
+      return new Person (statue.name, 'relieved')
+    }
   }
 }
 
